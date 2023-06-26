5c64bd1a56257dc71ba2921a65703b2fed5b27cb
Andrzej Knop <aknop07@gmail.com>
2023-06-26 20:11:40 +0200

    added functions

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
-console.log("Hello world!");
+{
+    const welcome = () => {
+        console.log("Hello world!");
+    };
 
-let button = document.querySelector(".js-button");
-let heading = document.querySelector(".js-image");
-let text = document.querySelector(".js-text");
+    const toggleBackground = () => {
+        const heading = document.querySelector(".js-image");
+        const text = document.querySelector(".js-text");
+        heading.classList.toggle("section__button--invisible");
+        heading.classList.contains("section__button--invisible") ? text.innerText = "Show" : text.innerText = "Remove";
+    };
 
+    const init = () => {
+        const button = document.querySelector(".js-button");
+        button.addEventListener("click", toggleBackground);
 
-button.addEventListener("click", () => {
-    heading.classList.toggle("section__button--invisible");
-    heading.classList.contains("section__button--invisible") ? text.innerText = "Show" : text.innerText = "Remove";
-});
+        welcome();
+    };
+
+    init();
+}
